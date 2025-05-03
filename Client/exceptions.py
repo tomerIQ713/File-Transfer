00340e6ed75782ca051c44aae277c285d0300e54
tomerIQ713 <114335797+tomerIQ713@users.noreply.github.com>
2025-05-03 13:51:09 +0300

    Delete Client directory

--- Client/exceptions.py
+++
@@ -1,7 +0,0 @@
-class ConnectionFailedError(Exception):
-    def __init__(self, *args):
-        super().__init__(*args)
-
-class InvalidPackageException(Exception):
-    def __init__(self, *args):
-        super().__init__(*args)
